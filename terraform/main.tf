b4c6aff2214bed9bb86ca3a1789ae18c0314af9a
raveheart1 <gordonmcw@gmail.com>
2025-01-18 18:23:29 -0500

    temporarily disables webapps until I can get the quota increased

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,59 +29,59 @@ resource "azurerm_storage_container" "api" {
   container_access_type = "private"
 }
 
-resource "azurerm_service_plan" "scoresage_asp" {
-  name                = "scoresage-asp"
-  location            = azurerm_resource_group.scoresage_rg.location
-  resource_group_name = azurerm_resource_group.scoresage_rg.name
-  os_type             = "Linux" 
-  sku_name            = "P1v2"   
-}
+# resource "azurerm_service_plan" "scoresage_asp" {
+#   name                = "scoresage-asp"
+#   location            = azurerm_resource_group.scoresage_rg.location
+#   resource_group_name = azurerm_resource_group.scoresage_rg.name
+#   os_type             = "Linux" 
+#   sku_name            = "F1"   
+# }
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "scoresage-app"
-  location            = azurerm_resource_group.scoresage_rg.location
-  resource_group_name = azurerm_resource_group.scoresage_rg.name
-  service_plan_id     = azurerm_service_plan.scoresage_asp.id
+# resource "azurerm_linux_web_app" "example" {
+#   name                = "scoresage-app"
+#   location            = azurerm_resource_group.scoresage_rg.location
+#   resource_group_name = azurerm_resource_group.scoresage_rg.name
+#   service_plan_id     = azurerm_service_plan.scoresage_asp.id
 
-  site_config {
-    application_stack {
-      python_version = "3.9"  # Python runtime version
-    }
-    always_on = true
-    ftps_state = "Disabled" # Secure the app by disabling FTP
-  }
+#   site_config {
+#     application_stack {
+#       python_version = "3.9"  # Python runtime version
+#     }
+#     always_on = true
+#     ftps_state = "Disabled" # Secure the app by disabling FTP
+#   }
 
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "python"
-    AzureWebJobsStorage      = azurerm_storage_account.scoresage_sa01.primary_connection_string
-    WEBSITE_RUN_FROM_PACKAGE = "1"
-  }
+#   app_settings = {
+#     FUNCTIONS_WORKER_RUNTIME = "python"
+#     AzureWebJobsStorage      = azurerm_storage_account.scoresage_sa01.primary_connection_string
+#     WEBSITE_RUN_FROM_PACKAGE = "1"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
 
-resource "azurerm_function_app_function" "example" {
-  name            = "example-function-app-function"
-  function_app_id = azurerm_linux_function_app.example.id
-  language        = "Python"
+# resource "azurerm_function_app_function" "example" {
+#   name            = "example-function-app-function"
+#   function_app_id = azurerm_linux_function_app.example.id
+#   language        = "Python"
 
-  # No file block yet, as main.py is not available
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function",
-        "direction" = "in",
-        "methods"   = ["get", "post"],
-        "name"      = "req",
-        "type"      = "httpTrigger"
-      },
-      {
-        "direction" = "out",
-        "name"      = "$return",
-        "type"      = "http"
-      }
-    ]
-  })
-}
+#   # No file block yet, as main.py is not available
+#   config_json = jsonencode({
+#     "bindings" = [
+#       {
+#         "authLevel" = "function",
+#         "direction" = "in",
+#         "methods"   = ["get", "post"],
+#         "name"      = "req",
+#         "type"      = "httpTrigger"
+#       },
+#       {
+#         "direction" = "out",
+#         "name"      = "$return",
+#         "type"      = "http"
+#       }
+#     ]
+#   })
+# }
